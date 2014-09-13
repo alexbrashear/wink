9c21dedad4d3c62d2f727ad26137336afcac96df
Connor Hailey <connorhailey@gmail.com>
2014-09-13 16:02:23 -0400

    commit

--- Wink/Wink/WConstants.m
+++ Wink/Wink/WConstants.m
@@ -11,4 +11,3 @@
 
 NSString * const PARSE_APPLICATION_ID = @"sxmSfFmRLYLlJ3RKEuSyTjgDZuOPyJ3Hcfk4Esca";
 NSString * const PARSE_CLIENT_KEY = @"fwtfnvf949H59G2SaY0M0r3USM4ZtSFtsHahQ1Wv";
-NSString * const HIPMOB_KEY = @"96a041ead0c642c8afaf45e9f89ee9d3";
