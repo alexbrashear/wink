236fd8ccf5c81d1837df8ac12a656b161becd9d8
Connor Hailey <connorhailey@gmail.com>
2014-09-12 21:26:02 -0400

    facebook login done

--- Wink/Wink/WelcomeViewController.m
+++ Wink/Wink/WelcomeViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "WelcomeViewController.h"
+#import <SVProgressHUD.h>
+#import <Parse/Parse.h>
+#import <ParseFacebookUtils/PFFacebookUtils.h>
 
 @interface WelcomeViewController ()
 
@@ -17,6 +20,94 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)connect:(id)sender {
+    [SVProgressHUD showWithStatus:@"Retrieving FB Info"];
+    
+    // Set permissions required from the facebook user account
+    NSArray *permissionsArray = @[ @"user_about_me", @"user_relationships", @"user_birthday", @"user_location", @"email"];
+    
+    // Login PFUser using facebook
+    [PFFacebookUtils logInWithPermissions:permissionsArray block:^(PFUser *user, NSError *error) {
+        
+        if (!user) {
+            if (!error) {
+                NSLog(@"Uh oh. The user cancelled the Facebook login.");
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Log In Error" message:@"Uh oh. The user cancelled the Facebook login." delegate:nil cancelButtonTitle:nil otherButtonTitles:@"Dismiss", nil];
+                [alert show];
+            } else {
+                NSLog(@"Uh oh. An error occurred: %@", error);
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Log In Error" message:[error description] delegate:nil cancelButtonTitle:nil otherButtonTitles:@"Dismiss", nil];
+                [alert show];
+            }
+        } else if (user.isNew) {
+            NSLog(@"User with facebook signed up and logged in!");
+            [self getUserInfo];
+        } else {
+            NSLog(@"User with facebook logged in!");
+        }
+    }];
+
+}
+
+-(void)getUserInfo{
+    NSString *requestPath = @"me/?fields=name,location,gender,birthday,relationship_status,picture,email,id";
+    
+    FBRequest *request = [[FBRequest alloc] initWithSession:[PFFacebookUtils session] graphPath:requestPath];
+    
+    [request startWithCompletionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+        if (!error) {
+            NSDictionary *userData = (NSDictionary *)result; // The result is a dictionary
+            
+            NSString *name = [userData objectForKey:@"name"];
+            NSString *email = [userData objectForKey:@"email"];
+            NSString *sID = [userData objectForKey:@"id"];
+            // get the FB user's profile image
+            NSDictionary *dicFacebookPicture = [userData objectForKey:@"picture"];
+            NSDictionary *dicFacebookData = [dicFacebookPicture objectForKey:@"data"];
+            NSString *sUrlPic= [dicFacebookData objectForKey:@"url"];
+            UIImage* imgProfile = [UIImage imageWithData:
+                                   [NSData dataWithContentsOfURL:
+                                    [NSURL URLWithString: sUrlPic]]];
+            
+            PFUser *user = [PFUser currentUser];
+            user.email = email;
+            [user setObject:[UIDevice currentDevice].identifierForVendor.UUIDString forKey:@"UUID"];
+            [user setObject:name forKey:@"name"];
+            [user setObject:sID forKey:@"fbID"];
+            [user setObject:imgProfile forKey:@"profPic"];
+            
+            [user saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+                if (succeeded) {
+                    [SVProgressHUD dismiss];
+                    [self performSegueWithIdentifier:@"toMain" sender:self];
+                    
+                } else {
+                    [SVProgressHUD dismiss];
+                    NSLog(@"fml: %@", error);
+                }
+            }];
+
+            // now request FB friend list
+            FBRequest *request = [[FBRequest alloc] initWithSession:[PFFacebookUtils session] graphPath:@"me/friends"];
+            
+            [request startWithCompletionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+                if (!error) {
+                    NSArray *data = [result objectForKey:@"data"];
+                    
+                    if (data) {
+                        //we now have an array of NSDictionary entries containing friend data
+                        for (NSMutableDictionary *friendData in data) {
+                            // do something interesting with the friend data...
+                            NSLog(@"friendData: %@", friendData);
+                        }
+                    }
+                    
+                }
+            }];
+        }
+    }];
+    
 }
 
 - (void)didReceiveMemoryWarning {
