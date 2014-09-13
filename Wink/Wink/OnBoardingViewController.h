c693a1039c0c767064977e74ff2c5123029b3ef9
Alex Brashear <alexjbrashear@gmail.com>
2014-09-12 22:29:45 -0400

    added page view controller stuff

---
+++ Wink/Wink/OnBoardingViewController.h
@@ -0,0 +1,13 @@
+//
+//  OnBoardingViewController.h
+//  Wink
+//
+//  Created by Alex Brashear on 9/12/14.
+//  Copyright (c) 2014 Nortron Technologies LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OnBoardingViewController : UIViewController
+
+@end
