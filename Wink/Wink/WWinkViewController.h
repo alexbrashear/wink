1a7521e815198921ebd755d6079735afd45e106f
Connor Hailey <connorhailey@gmail.com>
2014-09-12 21:56:54 -0400

    wink

---
+++ Wink/Wink/WWinkViewController.h
@@ -0,0 +1,13 @@
+//
+//  WWinkViewController.h
+//  Wink
+//
+//  Created by Connor on 9/12/14.
+//  Copyright (c) 2014 Nortron Technologies LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WWinkViewController : UIViewController
+
+@end
---
+++ Wink/Wink/WWinkViewController.m
@@ -0,0 +1,13 @@
+//
+//  WWinkViewController.m
+//  Wink
+//
+//  Created by Connor on 9/12/14.
+//  Copyright (c) 2014 Nortron Technologies LLC. All rights reserved.
+//
+
+#import "WWinkViewController.h"
+
+@implementation WWinkViewController
+
+@end
